8a01d3dc627b4ff1688c1bdbfd90d591d51d062b
Christopher Feghali <chrisfeghali@live.com>
2021-11-16 20:55:20 +0000

    comment warning import
    Signed-off-by: Christopher Feghali <chrisfeghali@live.com>

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {
   Route,
 } from "react-router-dom";
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
